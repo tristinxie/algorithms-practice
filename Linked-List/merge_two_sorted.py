3c2162d37118bb0650387114ab4006a91ea92374
tristinxie <tristinxie@gmail.com>
2021-04-06 00:34:05 -0700

    mergetwosorted with help

---
+++ Linked-List/merge_two_sorted.py
@@ -0,0 +1,25 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing 
+together the nodes of the first two lists.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = curr = ListNode(0)
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        
+        return dummy.next
